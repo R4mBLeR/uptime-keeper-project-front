1e1c5b14bdcb1cf0c8159b0b6d1884947d47cfcf
R4mBLe_ <misharad2008@gmail.com>
2026-07-07 01:47:20 +0300

    connect backend to register(minimal)

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,15 +1,37 @@
 import styles from './RegisterPage.module.css'
 import {useState} from "react";
 import Header from "../../components/layouts/Header/Header.tsx";
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const RegisterPage = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordLabel, setPasswordLabel] = useState(" ");
 
-    const handleSubmit = (e: any) => {
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
+
+        try {
+            const response = await axios.post(`${API_URL}/auth/register`, { username, email, password });
+
+            console.log('Успешно отправлено:', response.data);
+        } catch (error) {
+            console.error('Ошибка запроса:', error);
+        }
     };
+
+    const handleInputPassword = async (repeatPassword: string) => {
+        setPassword(password)
+        if(password != repeatPassword) {
+            setPasswordLabel("Passwords don't match.");
+        } else {
+            setPasswordLabel('');
+        }
+    };
+
     return (
         <>
             <Header/>
@@ -54,12 +76,12 @@ const RegisterPage = () => {
                             <label>Repeat password</label>
                             <input
                                 type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => handleInputPassword(e.target.value)}
                                 required
                                 className={styles.input}
                             />
                         </div>
+                        <label className={styles.passwordLabel}>{passwordLabel}</label>
                         <div className={styles.registerButtonContainer}>
                             <button type="submit" className={styles.registerButton}>
                                 Register
